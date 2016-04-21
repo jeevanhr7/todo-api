eee3ebb929a74d074526defae0c313889da2a92a
jeevanhr7 <jeevanhr7@gmail.com>
2016-04-21 17:42:35 +0530

    adde the Environement Variable and Postgress

--- db.js
+++ db.js
@@ -2,10 +2,19 @@
  * Created by jeevan on 4/21/2016.
  */
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-    'dialect': 'sqlite',
-    'storage': __dirname + '/data/basic-sqlite-database.sqlite'
-});
+var env=process.env.NODE_ENV||'development';
+if(env=='production')
+{
+    sequelize=new Sequelize(process.env.DATABASE_URL,{
+        dialect:'postgres'
+    });
+}else {
+     sequelize = new Sequelize(undefined, undefined, undefined, {
+        'dialect': 'sqlite',
+        'storage': __dirname + '/data/basic-sqlite-database.sqlite'
+    });
+}
+
 
 var db={};
 db.todo=sequelize.import(__dirname+'/models/todo.js');
